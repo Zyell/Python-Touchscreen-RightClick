bfd5762b50fbc304e9288ad45255170cdcc8de2b
Zyell <astroneer001@gmail.com>
2016-10-09 15:46:34 -0700

    Updated readme to include systemd service, proposed fix for issue #2

--- Python_Touchscreen_RightClick.py
+++ Python_Touchscreen_RightClick.py
@@ -21,8 +21,10 @@ class TrackedEvent(object):
     timing of long presses, and event completion.
     """
 
-    def __init__(self):
+    def __init__(self, abilities, var_x, var_y):
         """ Initialize tracking attributes. """
+        self.abilities = abilities
+        self.vars = {'ABS_X': var_x, 'ABS_Y': var_y}
         self.position = {'ABS_X': None, 'ABS_Y': None}
         self.slots = []
         self.fingers = 0
@@ -30,7 +32,7 @@ class TrackedEvent(object):
         self.discard = 0
         self.moved = 0
         self.track_start = None
-        self.click_delay = 1.7
+        self.click_delay = 1.5
 
     def add_finger(self, slot):
         """  Add a detected finger. """
@@ -65,8 +67,10 @@ class TrackedEvent(object):
         """ tracks position to track movement of fingers """
         if self.position[event_code] is None:
             self.position[event_code] = value
+            print(event_code, value)
         else:
-            self._moved_event()
+            if abs(self.position[event_code] - value) > self.vars[event_code]:
+                self._moved_event()
 
     def trackit(self):
         """ start timing for long press """
@@ -85,9 +89,7 @@ class TrackedEvent(object):
 
     def _initiate_right_click(self):
         """ Internal method for initiating a right click at touch point. """
-        capabilities = {ecodes.EV_ABS: (ecodes.ABS_X, ecodes.ABS_Y),
-                        ecodes.EV_KEY: (ecodes.BTN_LEFT, ecodes.BTN_RIGHT)}
-        with UInput(capabilities) as ui:
+        with UInput(self.abilities) as ui:
             ui.write(ecodes.EV_ABS, ecodes.ABS_X, 0)
             ui.write(ecodes.EV_ABS, ecodes.ABS_Y, 0)
             ui.write(ecodes.EV_KEY, ecodes.BTN_RIGHT, 1)
@@ -103,14 +105,35 @@ def initiate_gesture_find():
     """
     for device in list_devices():
         dev = InputDevice(device)
-        if (dev.name == 'ELAN Touchscreen') or (dev.name == 'Atmel Atmel maXTouch Digitizer'):
+        if (dev.name == 'ELAN Touchscreen') or \
+           (dev.name == 'Atmel Atmel maXTouch Digitizer'):
             break
-    codes = dev.capabilities()
     Abs_events = {}
+    abilities = {ecodes.EV_ABS: [ecodes.ABS_X, ecodes.ABS_Y],
+                 ecodes.EV_KEY: (ecodes.BTN_LEFT, ecodes.BTN_RIGHT)}
+    # Assuming QHD screen on my Yoga 2 Pro as default for resolution measures
+    res_x = 13  # touch unit resolution # units/mm in x direction
+    res_y = 13  # touch unit resolution # units/mm in y direction
+    # would be weird if above resolutions differed, but will treat generically
+    codes = dev.capabilities()
     for code in codes:
         if code == 3:
             for type_code in codes[code]:
-                Abs_events[type_code[0]] = ecodes.ABS[type_code[0]]
+                human_code = ecodes.ABS[type_code[0]]
+                if human_code == 'ABS_X':
+                    vals = type_code[1]
+                    abilities[ecodes.EV_ABS][0] = (ecodes.ABS_X, vals)
+                    res_x = vals[-1]
+                elif human_code == 'ABS_Y':
+                    vals = type_code[1]
+                    abilities[ecodes.EV_ABS][1] = (ecodes.ABS_Y, vals)
+                    res_y = vals[-1]
+                Abs_events[type_code[0]] = human_code
+    # Average  index finger width is 16-20 mm, assume 20 mm
+    # touch resolution noise assumed at 10% (5% radius), so 1.0 mm by default
+    # this seemed resonable from my own trial tests
+    var_x = 1.0 * res_x  # variablity in movement allowed in x direction
+    var_y = 1.0 * res_y  # variablity in movement allowed in y direction
 
     MT_event = None
     for event in dev.read_loop():
@@ -119,7 +142,7 @@ def initiate_gesture_find():
                 MT_event = None
         if event.type == ecodes.EV_ABS:
             if MT_event is None:
-                MT_event = TrackedEvent()
+                MT_event = TrackedEvent(abilities, var_x, var_y)
             event_code = Abs_events[event.code]
             if event_code == 'ABS_MT_SLOT':
                 MT_event.add_finger(event.value)
@@ -130,6 +153,7 @@ def initiate_gesture_find():
                     MT_event.remove_fingers()
                 else:
                     MT_event.trackit()
+
 
 if __name__ == '__main__':
     initiate_gesture_find()
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Python Touchscreen Right Click
 ======================
 
-This project implements right click functionality using a touchscreen on an Ubuntu system using the [Python evdev](https://github.com/gvalkov/python-evdev) library.  Because of unity's deep integration with multitouch gestures, it disallows many other systems from implementing basic gestures that (IMHO) are missing from the default multitouch gestures.  This script will not override unity's gestures, but fill in some missing ones, working alongside unity's multitouch system.
+This project implements right click functionality using a touchscreen on an Ubuntu system using the [Python evdev](https://github.com/gvalkov/python-evdev) library.  Because of unity's deep integration with multitouch gestures, it disallows many other systems from implementing basic gestures that (IMHO) are missing from the default multitouch gestures.  This script will not override unity's gestures, but fill in some missing ones, working alongside unity's multitouch system. (Also tested in gnome-shell given gnome-shell's incomplete implementation of right click touch)
 
 
 Implemented gestures
@@ -15,10 +15,32 @@ Two gesture options have been implemented for right click:
 Setting up script
 ---------------------
 
-Install requirements (python evdev).  Will work in either Python 2.7 or 3.4 *tested.
-This has been tested on both Ubuntu 14.04 and 15.04 with an ELAN/Atmel touchscreen on Lenovo Yoga 2 pro, and Surface Pro 2.
+Install requirements (python evdev).  Will work in either Python 2.7 or 3.4+ *tested.
+This has been tested on Ubuntu 14.04, 15.04, & 16.04 with an ELAN/Atmel touchscreen on Lenovo Yoga 2 pro, and Surface Pro 2.
 
-To modify the delay for your right click, open the script in a text editor and modify the self.click_delay variable, the default is 1.7 seconds.
+To modify the delay for your right click, open the script in a text editor and modify the self.click_delay variable, the default is 1.5 seconds.
+
+In order to launch the script at startup, the rc.local method may or may not work.  Using systemd, you can setup a service.  The service can be setup as follows to call a shell script:
+
+The shell script should contain something like:
+```
+#!/bin/sh
+python3 /(path to python script...)/Python_Touchscreen_RightClick.py
+```
+
+The service can be set up as follows:
+```
+[Unit]
+Description=script to appropriately set right click via touch
+Documentation=none
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/(sheel script from above...)
+
+[Install]
+WantedBy=multi-user.target
+
 
 Edit /etc/rc.local file to include launching this script.
 
