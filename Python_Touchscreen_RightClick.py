64cca5224817346bcba168a8143375bfa8724848
Zyell <astroneer001@gmail.com>
2016-10-09 21:06:57 -0700

    Code cleanup and simplification, long press menu rework request issue #2

--- Python_Touchscreen_RightClick.py
+++ Python_Touchscreen_RightClick.py
@@ -10,7 +10,7 @@ Currently implements 2 types of right click options:
 """
 
 from evdev import InputDevice, ecodes, UInput, list_devices
-import datetime
+from threading import Timer
 
 
 class TrackedEvent(object):
@@ -21,12 +21,12 @@ class TrackedEvent(object):
     timing of long presses, and event completion.
     """
 
-    def __init__(self, abilities, var_x, var_y):
+    def __init__(self, dev, abilities, var_x, var_y):
         """ Initialize tracking attributes. """
+        self.dev = dev
         self.abilities = abilities
         self.vars = {'ABS_X': var_x, 'ABS_Y': var_y}
         self.position = {'ABS_X': None, 'ABS_Y': None}
-        self.slots = []
         self.fingers = 0
         self.total_event_fingers = 0
         self.discard = 0
@@ -34,33 +34,38 @@ class TrackedEvent(object):
         self.track_start = None
         self.click_delay = 1.5
 
-    def add_finger(self, slot):
+    def add_finger(self):
         """  Add a detected finger. """
-        if slot not in self.slots:
-            self.fingers += 1
-            self.slots.append(slot)
-        if self.total_event_fingers < self.fingers:
-            self.total_event_fingers = self.fingers
+        self.fingers += 1
+        self.total_event_fingers = self.fingers
 
     def remove_fingers(self):
         """ Remove detected finger upon release. """
-        if self.total_event_fingers == self.fingers:
-            if self.total_event_fingers == 0:
-                self.total_event_fingers = 1
+        if self.fingers == 1:
             print('Total Fingers used: ', self.total_event_fingers)
         self.fingers -= 1
 
         if (self.fingers == 0 and
                 self.total_event_fingers == 2 and
                 self.moved == 0):
+            self.total_event_fingers = 0
             self._initiate_right_click()
 
-        elif ((self.fingers == 0 or self.fingers == -1) and
+        elif (self.fingers == 0 and
                 self.total_event_fingers == 1 and
                 self.moved == 0):
-            self._internal_timing()
+            self.total_event_fingers = 0
+            try:
+                self.track_start.cancel()
+                self.track_start.join()
+            except AttributeError:  # capture Nonetype track_start
+                pass
+            try:
+                self.dev.ungrab()
+            except OSError:  # capture case where grab was never initiated
+                pass
 
-        if self.fingers == 0 or self.fingers == -1:
+        if self.fingers == 0:
             self.discard = 1
 
     def position_event(self, event_code, value):
@@ -70,21 +75,23 @@ class TrackedEvent(object):
         else:
             if abs(self.position[event_code] - value) > self.vars[event_code]:
                 self._moved_event()
+        if (self.fingers == 1 and self.position['ABS_X'] and
+                self.position['ABS_Y'] and self.track_start is None):
+            self._trackit()
 
-    def trackit(self):
+    def _trackit(self):
         """ start timing for long press """
-        self.track_start = datetime.datetime.now()
+        self.track_start = Timer(self.click_delay, self._long_press)
+        self.track_start.start()
+
+    def _long_press(self):
+        if self.fingers == 1 and self.moved == 0:
+            self._initiate_right_click()
+            self.dev.grab()
 
     def _moved_event(self):
         """ movement detected. """
         self.moved = 1
-
-    def _internal_timing(self):
-        """ Internal method for determining long press time right clicking. """
-        if self.track_start is not None:
-            elapsed = datetime.datetime.now() - self.track_start
-            if elapsed.total_seconds() > self.click_delay:
-                self._initiate_right_click()
 
     def _initiate_right_click(self):
         """ Internal method for initiating a right click at touch point. """
@@ -118,6 +125,7 @@ def initiate_gesture_find():
     for code in codes:
         if code == 3:
             for type_code in codes[code]:
+
                 human_code = ecodes.ABS[type_code[0]]
                 if human_code == 'ABS_X':
                     vals = type_code[1]
@@ -136,22 +144,19 @@ def initiate_gesture_find():
 
     MT_event = None
     for event in dev.read_loop():
-        if MT_event:
-            if MT_event.discard == 1:
-                MT_event = None
         if event.type == ecodes.EV_ABS:
             if MT_event is None:
-                MT_event = TrackedEvent(abilities, var_x, var_y)
+                MT_event = TrackedEvent(dev, abilities, var_x, var_y)
             event_code = Abs_events[event.code]
-            if event_code == 'ABS_MT_SLOT':
-                MT_event.add_finger(event.value)
-            elif event_code == 'ABS_X' or event_code == 'ABS_Y':
+            if event_code == 'ABS_X' or event_code == 'ABS_Y':
                 MT_event.position_event(event_code, event.value)
             elif event_code == 'ABS_MT_TRACKING_ID':
                 if event.value == -1:
                     MT_event.remove_fingers()
+                    if MT_event.discard == 1:
+                        MT_event = None
                 else:
-                    MT_event.trackit()
+                    MT_event.add_finger()
 
 
 if __name__ == '__main__':
